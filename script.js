22907c3cadff0cf9135d13cc70c19f7ced1c908a
Ananyahs <39176552+Ananyahs@users.noreply.github.com>
2023-07-13 15:50:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -13,8 +13,4 @@ let cap1 ={
 
 // Write your code here, make sure the name of variables is exactly same as mentioned in the problem statement
 
-window.players = players;
-window.person = person;
-window.team = team;
-window.team1 = team1;
 
